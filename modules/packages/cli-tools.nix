665253faa7fb4315fd87d3ed00c111f94afa2dac
Edmondo Porcu <edmondo.porcu@gmail.com>
2025-12-29 21:18:20 -0500

    PR 02: CLI Tools Module (#2)
    * Separated modules
    
    * Remove unnecessary comments from cli-tools.nix
    
    Keep code clean and self-documenting - package names are clear enough

---
+++ modules/packages/cli-tools.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    bat
+    eza
+    fd
+    ripgrep
+    fzf
+    zoxide
+    tree
+    unzip
+    zip
+    file
+    which
+    lsof
+    btop
+    lm_sensors
+    pciutils
+    starship
+    atuin
+    wl-clipboard
+    gnupg
+    pass
+    ghostty
+    kitty
+    fastfetch
+    neofetch
+    curl
+    wget
+  ];
+}
